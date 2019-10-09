a2085d5b17018cf107fe510ac6852ff09dca65ca
Saurabh Narhe <snarhe@affinionds.com>
2019-10-09 11:10:24 +0530

    github api done.

---
+++ WebhookTest/Models/GithubPayload.cs
@@ -0,0 +1,12 @@
+﻿namespace WebhookTest.Models
+{
+    public class GithubPayload
+    {
+        public PullRequest pull_request;
+    }
+
+    public class PullRequest
+    {
+        public string review_comments_url;
+    }
+}
